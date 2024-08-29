e6edf4dd44625b3c7de870a8041fc7ef9aa5e9f6
Melike Avsan <104467626+melikeavsan@users.noreply.github.com>
2024-08-29 15:56:23 +0300

    Add files via upload

---
+++ vucut-kitle-indeksi-hesaplama.py
@@ -0,0 +1,34 @@
+def calculate_bmi(weight, height):
+    bmi = weight / (height ** 2)
+    return bmi
+
+def bmi_category(bmi):
+    if bmi < 18.5:
+        return "Zayıf"
+    elif 18.5 <= bmi < 24.9:
+        return "Normal"
+    elif 25 <= bmi < 29.9:
+        return "Fazla kilolu"
+    elif 30 <= bmi < 34.9:
+        return "1. Derece Obez"
+    elif 35 <= bmi < 39.9:
+        return "2. Derece Obez"
+    else:
+        return "3. Derece Obez"
+
+def main():
+    try:
+        weight = float(input("Kilonuzu kilogram cinsinden girin: "))
+        height = float(input("Boyunuzu metre cinsinden girin (örneğin, 1.75): "))
+        
+        bmi = calculate_bmi(weight, height)
+        category = bmi_category(bmi)
+        
+        print(f"Vücut Kitle Endeksiniz (BMI): {bmi:.2f}")
+        print(f"BMI Kategorisi: {category}")
+    
+    except ValueError:
+        print("Lütfen geçerli bir sayı girin.")
+
+if __name__ == "__main__":
+    main()
